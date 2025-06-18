1a6435058b5ac9b9c6208324905d3d21f5991970
Everett Merrill <everettwarrenmerrill@gmail.com>
2025-06-17 20:50:01 -0700

    "Added new folder and file for newProjectPage"

---
+++ client/src/components/newProjectPage/newProjectPage.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import type { DropdownItem } from '../../../types';
+import styles from './newProjectPage.module.css';
+
